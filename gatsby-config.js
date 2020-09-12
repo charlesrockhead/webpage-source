f73ece0e0bc8dee8eca5095bb1f72c0f3e5c56f3
Charles Rockhead <cmr@Marios-MBP.lan>
2020-09-12 15:57:11 -0500

    author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Mukund Venkateswaran`,
+    title: `Charles Rockhead`,
     description: `This is my personal webpage.`,
     author: `@gatsbyjs`,
   },
